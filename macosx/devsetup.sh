83015d0662eab96fd68f603a298858961672f321
kiban18 <kiban18@kiban18ui-MacBook-Pro.local>
2012-12-10 14:43:28 +0900

    for mac

--- macosx/devsetup.sh
+++ macosx/devsetup.sh
@@ -9,12 +9,20 @@ export aosp=$android/aosp
 export doc=$android/doc
 export tool=$android/tool
 
-alias lunch.android-4.2.1_r1='cd $aosp/android-4.2.1_r1 && . build/envsetup.sh && lunch full-eng'
+alias lunch.android-4.2.1_r1='cd $aosp/android-4.2.1_r1 && . build/envsetup.sh && lunch full-eng && source.devsetup'
 
 export adt_bundle_mac=/Applications/adt-bundle-mac
 export sdk=$adt_bundle_mac/sdk
 export skindir=$sdk/platforms/android-16/skins
+sdcard=~/.sdcard/sdcard.img
+emulator_cmd_common="emulator -sysdir $OUT -data $OUT/userdata.img -sdcard $sdcard -memory 1024 -gpu on -camera-front webcam0"
 alias ls.skins='l $skindir'
-alias emulator.build='emulator -sysdir $OUT -data $OUT/userdata.img -skindir $skindir'
+alias emulator.build='$emulator_cmd_common'
+alias emulator.build.skin='$emulator_cmd_common -skindir $skindir -skin '
+
+alias make.ctags='ctags -B -F -R --exclude="^out"'
+alias make.cscope='$envsetup/makecscope.sh'
+alias make.filelist='rm filelist'
+alias makedb='make.ctags; make.cscope; make.filelist'
 
 echo ~/.envsetup/$os/devsetup.sh sourced!!!
